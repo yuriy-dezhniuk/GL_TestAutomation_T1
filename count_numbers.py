b278658474f463fd90a6533e56dec1b97f4e72a5
yuriy-dezhniuk <33997334+yuriy-dezhniuk@users.noreply.github.com>
2021-10-08 21:52:57 +0300

    Add answer for Practical task 1.
    Practical task 1.
    Write a script (""count_numbers"") to calculate and print out the sum of all numbers in the file named "numbers.txt", ignoring lines that are empty or start with '#' symbol.
    ====== number.txt=========
    # 1000000 <-- do not count this
    123.4
    -34.21
    56.71000444.45
    -555.72
    +234.2
    false
    False 2v
    
    ========================
    Requirement 1. Create or use your GITHUB repo. Push code there and provide link to this repo as an answer;
    Requirement 2. Your code runs without Exceptions;
    Requirement 3. Lines that start with '#' symbol shall be ignored;
    Requirement 4. Empty lines shall be ignored;
    Requirement 5. Numbers can be positive and negative. e.g. "-1+2=1";
    Requirement 6. Use your main language;
    Requirement 7. Do the best you can. Do not spend more than 25 minutes. Push the code even if not all requirements are completed

---
+++ count_numbers.py
@@ -0,0 +1,12 @@
+import re
+fhand = open('number.txt', 'r', encoding = 'utf8')
+number_template = r'^[-+]?[0-9]*\.?[0-9]+$'
+sum = 0
+for line in fhand:
+    if line[0] != '#' and line != '\n':
+        res = re.findall(number_template, line)
+        if len(res) > 0:
+            for i in res:
+                sum += float(i)
+print(sum)
+
